8e29a4592172443979e9dc2d8c542b1427830a67
Rhaelkan <hakandoruk2k@gmail.com>
2023-11-03 15:42:08 +0300

    Companies section has been added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Explore from "@/components/Explore";
 import Hero from "@/components/Hero";
+import Companies from "@/components/Companies";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Explore />
+      <Companies />
     </main>
   );
 }
